2812f3ee9f0397d9ae59d16054599bc849172248
JSand89 <javier.esand@gmail.com>
2025-07-01 20:29:06 -0500

    primeros componentes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+function Navbar(){
+    return(
+        <>           
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Primeros Pasos</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">REACT</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Bootstrap</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+        </>
+    )
+}
+export default Navbar
