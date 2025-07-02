01334f1fdfa8d52c23dd61dbef975bced9e1a698
JSand89 <javier.esand@gmail.com>
2025-07-01 21:41:39 -0500

    personalizando css

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,10 @@
 import imagenTest from "../assets/imagenTest.jpg"
+import styles from "./features.module.css"
 export default function Features(){
     return(
     <>
-        <section id="features">
-            <h2>Caracteristicas Principales</h2>
+        <h2>Caracteristicas Principales</h2>
+        <section id="features" className={styles.features}>
             <div class="card" style={{width:"18rem"}}>
                 <img src={imagenTest} class="card-img-top" alt="..."/>
                 <div class="card-body">
@@ -11,7 +12,13 @@ export default function Features(){
                 </div>
             </div>
             <div class="card" style={{width:"18rem"}}>
-                <img src="..." class="card-img-top" alt="..."/>
+                <img src={imagenTest} class="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                </div>
+            </div>
+            <div class="card" style={{width:"18rem"}}>
+                <img src={imagenTest} class="card-img-top" alt="..."/>
                 <div class="card-body">
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 </div>
